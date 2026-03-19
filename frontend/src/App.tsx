7f5e1968c676db9cbc3d303811b44f9476d59027
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2026-03-18 21:22:35 -0300

    :sparkles: feat: add routing with react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+    </ Routes>
   )
 }
 
